6762a1b068d49b7101c6700af12aba09a2294f89
EdwardApollo <51210836+EdwardApollo@users.noreply.github.com>
2021-04-12 18:41:17 -0400

    Increase ki by roughly 10%

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -218,7 +218,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
       tire_stiffness_factor = 0.8467
       kps = 1.0
-      kis = 1.1
+      kis = 1.2
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2*kps, 0.8*kps, 0.5*kps]
       ret.longitudinalTuning.kiBP = [0., 35.]
