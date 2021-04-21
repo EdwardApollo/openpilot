5f3ab3369e8cb2710d45b268f46c224c727d06eb
EdwardApollo <51210836+EdwardApollo@users.noreply.github.com>
2021-04-20 20:25:30 -0400

    modify low end dynamioc follow

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -94,8 +94,8 @@ class LongitudinalMpc():
 
     # Calculate mpc
     t = sec_since_boot()
-    if v_ego == 0: #If at 0 mph
-      TR = 2.3+((12.235-v_ego)*0.03269) #follow distance in seconds
+    if v_ego <= 0.67056: #If at 0 mph
+      TR = 2.3 #follow distance in seconds
     elif v_ego <= 1.34112: #If below 3 mph
       TR = 1.6 #follow distance in seconds
     elif v_ego <= 6.7056: #If below 15 mph
